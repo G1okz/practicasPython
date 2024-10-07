6f8bacad505c79acd77aef43ae87dc2a58286b72
G1okz <miguelreynac1@gmail.com>
2024-10-07 11:36:20 +0200

    Refactor input validation to disallow numbers and spaces in the text string

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,5 @@
 """
-Ingresar una cadena de texto que solo acepte de tipo string y no acepte numeros.
+Ingresar una cadena de texto que solo acepte de tipo string y no acepte numeros ni espacios en blanco.
 Despues mostrar por pantalla la longitud de la cadena.
 """
 
@@ -7,7 +7,7 @@ print("Escriba la cadena de texto: ")
 
 cadena = input()
 
-# Verificar que la entrada no esté vacía, sea de tipo string y no contenga números
+# Verificar que la entrada no esté vacía, sea de tipo string y no contenga números o espacios
 if not isinstance(cadena, str) or not cadena.isalpha():
     print("Cadena de texto vacía, no válida o contiene números")
 else:
